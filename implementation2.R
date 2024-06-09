f3cb14a94141b64f384c10c4e084be6da3c52313
Thiago Tavares <thiago.tlopess@gmail.com>
2024-06-09 03:04:26 -0300

    cheking log likelihood

--- implementation2.R
+++ implementation2.R
@@ -2,36 +2,57 @@
 
 # PROBABILITY DENSITY FUNCTION
 
-dUGo <- function(x, mu=.5, sigma=1, tau=.5, log = FALSE)
+dUGo <- function(x, mu=.5, sigma=1.2, tau=.5)
 {
   
   fx1<- log(tau)/(1 - mu^(-sigma)) * sigma * x^(-(1 + sigma)) * exp((log(tau) / (1 - mu^(-sigma))) *
                                                    (1 - x^(-sigma)))
-  if(log==FALSE) fx<-fx1 else fX<-log(fx1)
-  return(fx)
+  
+  return(fx1)
   
 }
 
-integrate(dUGo,0,1) #VERIFICAR A INTEGRAL AQUI
-
-
+integrate(dUGo,0,1) 
 
 # CUMULATIVE DISTRIBUTION FUNCTION 
 
-pUGo <-  function(q, mu=.5, sigma=1, tau=.5) 
+pUGo <-  function(q, mu=.5, sigma=1.2, tau=.5) 
 {
   cdf <- exp(((log(tau)) / (1 - mu^(-sigma))) * (1 - q^(-sigma)))
   return(cdf)
 }
 
 
-
 # QUANTILE FUNCTION
-qUGo<-function(u, mu=.5, sigma=1, tau=.5)
+qUGo<-function(u, mu=.5, sigma=1.2, tau=.5)
 {
   q<-(1 - (log(u) * (1 - mu^(-sigma)) / log(tau)))^(-1 / sigma)
   return(q)
 }
+
+
+# CHEKING LOG-LIKELIHOOD 
+
+x= .5 
+mu=.5 
+sigma=1.2 
+tau=.5
+
+
+llik<-function(x, mu=.5, sigma=1, tau=.5)
+{
+  ll<-log(log(tau)/(1-mu^-sigma))+log(sigma)+(-1-sigma)*log(x)+(log(tau)/(1-mu^-sigma))*(1-x^-sigma)
+  return(ll)
+}
+
+log(dUGo(x, mu=.5, sigma=1, tau=.5))
+llik(x, mu=.5, sigma=1, tau=.5)
+
+z=c(.3,.4,.5)
+
+log(dUGo(z[1], mu=.5, sigma=1, tau=.5)*dUGo(z[2], mu=.5, sigma=1, tau=.5)*dUGo(z[3], mu=.5, sigma=1, tau=.5))
+
+sum(llik(z, mu=.5, sigma=1, tau=.5))
 
 
 
